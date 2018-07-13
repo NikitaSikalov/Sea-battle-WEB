382dbcdea21a53c6be9bca1d132fdaee3632c2d9
sikalovnikita <sikalov.ns@phystech.edu>
2018-07-14 01:23:37 +0300

    добавил искуственный интллект

--- script_of_batle.js
+++ script_of_batle.js
@@ -156,12 +156,11 @@ window.onload = function () {
         }
         this.player.squadron.push(this);
         if (this.player == user) this.showShip();
-        //this.showShip();
         if (user.squadron.length == 10) {
             getElement('play').setAttribute('data-hidden', 'false');
         }
     };
-    
+
     Ships.prototype.showShip = function () {
         var div = document.createElement('div'),
             dir = (this.kx == 1) ? ' vertical' : '',
@@ -194,15 +193,44 @@ window.onload = function () {
            init: function () {
                self = this;
                var rnd = getRandom(1);
-
-               rnd = 0;
-
                player = (rnd == 0) ? user : comp;
                enemy = (rnd == 0) ? comp : user;
+
+               /////////////////////////////////////////////////////////////
+               // координаты которые не входят в shootMatrixAI и где потенциально может быть корабль
+               comp.shootMatrix = [];
+               // диагнональные клетки с которых мы начнем обстрел
+               comp.shootMatrixAI = [];
+               // массив в влектами вокруг клетки с попаданием
+               comp.shootMatrixAround = [];
+               // массив начала побочных диаганалей
+               comp.startPoints = [
+                   [ [6, 0], [2, 0], [0, 2], [0, 6] ],
+                   [ [3, 0], [7, 0], [9, 2], [9, 6] ]
+               ];
+               // информация по обстреленному кораблю
+               comp.tempShip = {
+                   // кол-во попаданий в корабль
+                   totalHits: 0,
+                   // первое и второе попадание
+                   firstHit: {},
+                   nextHit: {},
+                   kx: 0,
+                   ky: 0
+               };
+
+               self.setShootMatrix();
+                /////////////////////////////////////////////////////////
+
                if (player == user) {
                    compfield.addEventListener('click', self.shoot);
                    compfield.addEventListener('contextmenu', self.setEmptyCell);
                    self.showServiseText('Вы стреляете первым');
+               } else {
+                   self.showServiseText('Компьютер ходит первым');
+                   setTimeout(function () {
+                      return self.shoot();
+                   }, 1300);
                }
            },
 
@@ -217,8 +245,6 @@ window.onload = function () {
                // убираем событие браузера на клик прав кнопки мыши
                e.preventDefault();
                coords = self.transformCoordinates(e, comp);
-               //var ch = self.checkCell();
-               // ch == true => клетка пустая, иначе false
                if (comp.matrix[coords.x][coords.y] == 0 || comp.matrix[coords.x][coords.y] == 1) {
                     self.showIcons(enemy, coords, 'shaded-cell');
                     comp.matrix[coords.x][coords.y] = 2;
@@ -237,35 +263,12 @@ window.onload = function () {
                }
            },
 
-           //////////////////////////////////////
-           checkCell: function () {
-               var icons = enemy.querySelectorAll('icon-field'),
-                   flag = true;
-               [].forEach.call(icons, function (el) {
-                   var x = el.style.top.slice(0, -2) / comp.shipSide,
-                       y = el.style.left.slice(0, -2) / comp.shipSide;
-                   // Вроде не нужна
-               })
-           },
-           ////////////////////////////////////////////
-
            shoot: function(e) {
                if (e != undefined) {
                    if (e.which != 1) return false;
                    coords = self.transformCoordinates(e, enemy);
                } else {
-                   var temp = {
-                       x: getRandom(9),
-                       y: getRandom(9)
-                   };
-                   //temp.x = getRandom(10);
-                   //temp.y = getRandom(10);
-                   while (enemy.matrix[temp.x][temp.y] != 0 && enemy.matrix[temp.x][temp.y] != 1) {
-                       temp.x = getRandom(9);
-                       temp.y = getRandom(9);
-                   }
-                   coords = temp;
-                  // coords = (comp.shootMatrixAround.length) ? self.getCoordinatesShotAround() : self.getCoordinatesShot();
+                   coords = self.getCoordinatesShot();
                }
 
                var val = enemy.matrix[coords.x][coords.y];
@@ -282,6 +285,10 @@ window.onload = function () {
                            // удаляем обработчики собтий для пользователя
                            compfield.removeEventListener('click', self.shoot);
                            compfield.removeEventListener('contextmenu', self.setEmptyCell);
+                           if (comp.shootMatrixAround.length == 0) {
+                               self.resetTempShip();
+                           }
+
                            setTimeout(function () {
                                return self.shoot();
                            }, 1300);
@@ -298,14 +305,18 @@ window.onload = function () {
                        text = (player == user) ? "Вы попали! Снова Ваш выстрел" : "Компьютер попал. Он снова ходит";
                        enemy.matrix[coords.x][coords.y] = 4;
                        self.showServiseText(text);
-                       
-                       for (var i = enemy.squadron.length - 1; i >=0; i--) {
+
+                       for (var i = enemy.squadron.length - 1; i >= 0; i--) {
                            var warship = enemy.squadron[i],
                                arrayDescks = enemy.squadron[i].matrix;
                            for (var j = 0; j < arrayDescks.length; j++) {
                                if (arrayDescks[j][0] == coords.x && arrayDescks[j][1] == coords.y) {
                                    warship.hits++;
                                    if (warship.hits == warship.decks) {
+                                       if (player == comp) {
+                                           comp.tempShip.x0 = warship.x0;
+                                           comp.tempShip.y0 = warship.y0;
+                                       }
                                        enemy.squadron.splice(i, 1);
                                    }
                                }
@@ -318,31 +329,56 @@ window.onload = function () {
                            if (player == user) {
                                compfield.removeEventListener('click', self.shoot);
                                compfield.removeEventListener('contextmenu', self.setEmptyCell);
-
-                               getElement("main_field_user").classList.remove('field');
-                               getElement("main_field_user").classList.add('dasha_style');
-                               getElement("main_field_user").classList.add('ships');
-                               getElement('main_field_user').removeChild(getElement('field_user'));
-
-                               getElement('main_field_comp').classList.add('roza');
-                               getElement('main_field_comp').classList.add('ships');
-                               getElement('main_field_comp').classList.remove('field');
-                               getElement('main_field_comp').removeChild(getElement('field_comp'));
                            } else {
-                               // показываем оставшиеся корабли
+                               comp.showShip();
                            }
-                       }
+                       } else {
+                           if (player == comp) {
+                               comp.tempShip.totalHits++;
+                               var points = [
+                                   [coords.x - 1, coords.y - 1],
+                                   [coords.x + 1, coords.y + 1],
+                                   [coords.x - 1, coords.y + 1],
+                                   [coords.x + 1, coords.y - 1]
+                               ];
+                               self.markEmptyCell(points);
+                               var max = self.checkMaxDecks();
+                               if (comp.tempShip.totalHits >= max) {
+                                   // однопалубный корабль
+                                   if (comp.totalHits == 1) {
+                                       points = [
+                                           [comp.tempShip.x0 - 1, comp.tempShip.y0],
+                                           [comp.tempShip.x0 + 1, comp.tempShip.y0],
+                                           [comp.tempShip.x0, comp.tempShip.y0 - 1],
+                                           [comp.tempShip.x0, comp.tempShip.y0 + 1]
+                                       ];
+                                   } else {
+                                       // многопалубный корабль
+                                       var x1 = comp.tempShip.x0 - comp.tempShip.kx,
+                                           y1 = comp.tempShip.y0 - comp.tempShip.ky,
+                                           x2 = comp.tempShip.x0 + comp.tempShip.kx * comp.tempShip.totalHits,
+                                           y2 = comp.tempShip.y0 + comp.tempShip.ky * comp.tempShip.totalHits;
+                                       points = [
+                                           [x1, y1],
+                                           [x2, y2]
+                                       ];
 
-                       if (player == comp) {
-                           setTimeout(function () {
-                               return self.shoot();
-                           }, 1300);
+                                   }
+                                   self.markEmptyCell(points);
+                                   self.resetTempShip();
+                               } else {
+                                   self.setShootMatrixAround();
+                               }
+                               setTimeout(function () {
+                                   return self.shoot();
+                               }, 1300);
+                           }
                        }
                        break;
 
                    // отмеченная координата
                    case 2:
-                       text = "Сначала снимите блокировку с этих координат!";
+                       text = "Сначла снимите блокировку с этих координат!";
                        self.showServiseText(text);
                        var icons = enemy.field.querySelectorAll('.shaded-cell');
                        [].forEach.call(icons, function (el) {
@@ -386,6 +422,140 @@ window.onload = function () {
                div.style.cssText = 'left: ' + (coords.y * enemy.shipSide) + 'px; top: ' + (coords.x * enemy.shipSide) + 'px;';
                enemy.field.appendChild(div);
 
+           },
+
+           setShootMatrix: function () {
+               for (var i = 0; i < 2; ++i) {
+                   for (var j = 0; j < comp.startPoints[i].length; ++j) {
+                       var x = comp.startPoints[i][j][0],
+                           y = comp.startPoints[i][j][1];
+                       switch (i) {
+                           // побочные диаганали с направлением вниз
+                           case 0:
+                               while(x <= 9 && y <= 9) {
+                                   comp.shootMatrixAI.push([x, y]);
+                                   x++;
+                                   y++;
+                                  // x = (x <= 9) ? x: 9;
+                                  // y = (y <= 9) ? y: 9;
+                               }
+                               break;
+                           case 1:
+                               while (x >= 0 && y <= 9) {
+                                   comp.shootMatrixAI.push([x, y]);
+                                   x--;
+                                   y++;
+                               }
+                               break;
+                       }
+                   }
+               }
+               for (var i = 0; i < 10; ++i) {
+                   for (var j = 0; j < 10; ++j) {
+                       comp.shootMatrix.push([i, j]);
+                   }
+               }
+               function compareRandom (a, b) {
+                    return Math.random() - 0.5;
+               }
+               comp.shootMatrixAI.sort(compareRandom);
+               comp.shootMatrix.sort(compareRandom);
+           },
+
+           getCoordinatesShot: function () {
+               var temp_coords = (comp.shootMatrixAround.length > 0) ? comp.shootMatrixAround.pop() :
+                   (comp.shootMatrixAI.length > 0) ? comp.shootMatrixAI.pop() : comp.shootMatrix.pop();
+               var obj = {
+                   x: temp_coords[0],
+                   y: temp_coords[1]
+               };
+               if (comp.shootMatrixAI.length != 0) {
+                   self.deleteElementMatrix(comp.shootMatrixAI, obj);
+               }
+               self.deleteElementMatrix(comp.shootMatrix, obj);
+               return obj;
+           },
+
+           deleteElementMatrix: function (array, obj) {
+               for(var i = 0; i < array.length; ++i) {
+                   if (array[i][0] == obj.x && array[i][1] == obj.y) {
+                       array.splice(i, 1);
+                       break;
+                   }
+               }
+           },
+
+           resetTempShip: function () {
+               comp.shootMatrixAround = [];
+               comp.tempShip = {
+                   totalHits: 0,
+                   firstHit: {},
+                   nextHit: {},
+                   kx: 0,
+                   ky: 0
+               };
+           },
+
+           markEmptyCell: function (points) {
+               for (var i = 0; i < points.length; i++) {
+                   var obj = {
+                       x: points[i][0],
+                       y: points[i][1]
+                   };
+                   if (obj.x < 0 || obj.x > 9 || obj.y < 0 || obj.y > 9) continue;
+                   if (user.matrix[obj.x][obj.y] != 0) continue;
+                   user.matrix[obj.x][obj.y] = 2;
+                   self.showIcons(user, obj, 'shaded-cell');
+                   self.deleteElementMatrix(comp.shootMatrix, obj);
+                   self.deleteElementMatrix(comp.shootMatrixAround, obj);
+                   self.deleteElementMatrix(comp.shootMatrixAI, obj);
+               }
+           },
+
+           checkMaxDecks: function () {
+               var arr = [];
+               for (var i = 0; i < user.squadron.length; i++) {
+                   arr.push(user.squadron[i].decks);
+               }
+               return Math.max.apply(null, arr);
+           },
+
+           setShootMatrixAround: function () {
+               if (comp.tempShip.kx == 0 && comp.tempShip.ky == 0) {
+                   if (Object.keys(comp.tempShip.firstHit).length == 0) {
+                       comp.tempShip.firstHit = coords;
+                   } else {
+                       comp.tempShip.nextHit = coords;
+                       comp.tempShip.kx = (comp.tempShip.firstHit.x != comp.tempShip.nextHit.x);
+                       comp.tempShip.ky = (comp.tempShip.firstHit.y != comp.tempShip.nextHit.y);
+
+                   }
+               }
+
+               if (coords.x > 0 && comp.tempShip.ky == 0) {
+                   if (user.matrix[coords.x - 1][coords.y] <= 1) {
+                       comp.shootMatrixAround.push([coords.x - 1, coords.y]);
+                   }
+               }
+               if (coords.x < 9 && comp.tempShip.ky == 0) {
+                   if (user.matrix[coords.x + 1][coords.y] <= 1) {
+                       comp.shootMatrixAround.push([coords.x + 1, coords.y]);
+                   }
+               }
+               if (coords.y > 0 && comp.tempShip.kx == 0) {
+                   if (user.matrix[coords.x][coords.y - 1] <= 1) {
+                       comp.shootMatrixAround.push([coords.x, coords.y - 1]);
+                   }
+               }
+               if (coords.y < 9 && comp.tempShip.kx == 0) {
+                   if (user.matrix[coords.x][coords.y + 1] <= 1) {
+                       comp.shootMatrixAround.push([coords.x, coords.y + 1]);
+                   }
+               }
+
+               if (comp.shootMatrixAround.length == 0) {
+                   self.resetTempShip();
+               }
            }
 
        };
